afee039317efb37d24c418c107506880c10259c9
Russell Jew <russelljew104@gmail.com>
2015-03-15 19:58:34 -0700

    Program mostly finished. One bug in program - Doesn't display uncommon digit if that digit occurs twice in one integer.

---
+++ cis25Spring2015SampleMidtermProblem3/cis25Spring2015SampleMidtermProblem3.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+
+//Function Prototype
+
+int displayMenu(void);
+void storeArrayElements(int*, int);
+void displayDigitProblem3RussellJew(int*, int);
+
+int main() {
+    int option;
+
+    do
+    {
+        option = displayMenu();
+    } while (option != 2);
+
+    return 0;
+}
+
+//Function Definitions
+
+int displayMenu() {
+    int option;
+    int* iPtr;
+    int size;
+
+    cout << "***********************************************" << endl;
+    cout << "*                    MENU 03                  *" << endl;
+    cout << "* 1. Calling displayDigitProblem3RussellJew() *" << endl;
+    cout << "* 2. Quit                                     *" << endl;
+    cout << "***********************************************" << endl;
+
+    cout << "Select an option (1 or 2): ";
+    cin >> option;
+    cout << endl;
+
+    switch (option) {
+    case 1:
+        cout << "  How many integers? ";
+        cin >> size;
+
+        iPtr = new int[size];
+
+        storeArrayElements(iPtr, size);
+
+        cout << "\n  Calling displayDigitProblem3RussellJew() -" << endl << endl;
+
+        displayDigitProblem3RussellJew(iPtr, size);
+
+        break;
+    case 2:
+        cout << "\n  Fun Excercise ..." << endl;
+        break;
+    default:
+        cout << "WRONG OPTION!\n\n\n";
+    }
+
+    return option;
+}
+
+void storeArrayElements(int* iPtr, int size) {
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+        cout << "    Enter integer #" << i + 1 << ": ";
+        cin >> *(iPtr + i);
+    }
+}
+
+void displayDigitProblem3RussellJew(int* iPtr, int size) {
+    int digits[10] = {0};
+    int i;
+    int digit;
+    int *iPtrTemp;
+    int numberOfUncommonEvenDigits = 0;
+
+    iPtrTemp = new int[size];
+
+    for (i = 0; i < size; i++) {
+        *(iPtrTemp + i) = (*(iPtr + i) < 0) ? -(*(iPtr + i)) : *(iPtr + i);
+    }
+
+    for (i = 0; i < size; i++) {
+        do {
+            digit = *(iPtrTemp + i) % 10;
+            
+            if (digit % 2 == 0) {
+                if (digits[digit] > 0)
+                    digits[digit] = -digits[digit];
+
+                if (digits[digit] == 0)
+                    digits[digit]++;
+            }
+
+            *(iPtrTemp + i) /= 10;
+        } while (*(iPtrTemp + i) != 0);
+    }
+
+    for (i = 0; i < 10; i += 2) {
+        if (digits[i] > 0)
+            numberOfUncommonEvenDigits++;
+    }
+
+    cout << "    From all of the given integers," << endl;
+    cout << "\n      There is/are " << numberOfUncommonEvenDigits << " uncommon even digit(s)." << endl;
+    cout << "\n      The uncommon even digit(s) is/are" << endl;
+
+    for (i = 0; i < 10; i += 2) {
+        if (digits[i] > 0)
+            cout << "        " << i << endl;
+    }
+
+    cout << endl << endl; 
+
+    delete[] iPtrTemp;
+}
