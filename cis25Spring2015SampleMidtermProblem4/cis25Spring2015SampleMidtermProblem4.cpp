f069cdb7a7bb6977e68700c5874fcf0a993543ac
Russell Jew <russelljew104@gmail.com>
2015-03-21 22:20:28 -0700

    Added comments.

--- cis25Spring2015SampleMidtermProblem4/cis25Spring2015SampleMidtermProblem4.cpp
+++ cis25Spring2015SampleMidtermProblem4/cis25Spring2015SampleMidtermProblem4.cpp
@@ -103,25 +103,26 @@ void updateBasedOnEvenUncommonRussellJew(int* iPtr, int size) {
     iPtrTemp = new int[size];
     iPtrTemp2 = new int[size];
 
+    //initialize iptrTemp
     for (i = 0; i < size; i++) {
         *(iPtrTemp + i) = (*(iPtr + i) < 0) ? -(*(iPtr + i)) : *(iPtr + i);
     }
 
     for (i = 0; i < size; i++) {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 10; j++) { //initialize flag array
             flag[j] = {false};
         }
 
         do {
             digit = *(iPtrTemp + i) % 10;
 
-            if (digit % 2 == 0) {
+            if (digit % 2 == 0) { // if digit is even
                 if (digits[digit] > 0 && flag[digit] == false) //false means it was greater than one before the for loop
-                    digits[digit] = -digits[digit];
+                    digits[digit] = -digits[digit]; // negative means it is not uncommon
 
-                if (digits[digit] == 0) {
+                if (digits[digit] == 0) { // if digit is a first time encountered
                     digits[digit]++;
-                    flag[digit] = true;
+                    flag[digit] = true; // so that if the digit occurs twice in a number it won't become negative
                 }
             }
 
@@ -129,21 +130,23 @@ void updateBasedOnEvenUncommonRussellJew(int* iPtr, int size) {
         } while (*(iPtrTemp + i) != 0);
     }
 
-    numberOfUncommonEvenDigits = new int[size];
+    numberOfUncommonEvenDigits = new int[size]; // parallel array to hold number of in each iPtr uncommon digits
 
-    for (i = 0; i < 10; i += 2) {
+    for (i = 0; i < 10; i += 2) {//to control the uncommon digits
         if (digits[i] > 0) { // If uncommon digit
 
+            //Reinitilize each array because of the /= operator for the digits
             for (k = 0; k < size; k++) {
                 *(iPtrTemp2 + k) = (*(iPtr + k) < 0) ? -(*(iPtr + k)) : *(iPtr + k);
             }
 
+            //Step through each element, keeping track of how many uncommon even digits appear for each value
             for (j = 0; j < size; j++) {
                 do {
                     digit = *(iPtrTemp2 + j) % 10;
 
                     if (digit == i)
-                        (*(numberOfUncommonEvenDigits + j))++;
+                        (*(numberOfUncommonEvenDigits + j))++; // increment if array value contains uncommon digit
 
                     *(iPtrTemp2 + j) /= 10;
                 } while (*(iPtrTemp2 + j) != 0);
@@ -154,17 +157,21 @@ void updateBasedOnEvenUncommonRussellJew(int* iPtr, int size) {
 
     for (i = 0; i < size; i++) {
 
-        largestUncommonOccurence = i;
+        largestUncommonOccurence = i;// set the intial largest uncommon occurence to i because will skip front
 
+        //Find the index for the iPtr value that has the most uncommon digits
         for (j = i + 1; j < size; j++) {
             if (*(numberOfUncommonEvenDigits + largestUncommonOccurence) < *(numberOfUncommonEvenDigits + j))
                 largestUncommonOccurence = j;
         }
 
+        //Swap the numberOfUncommonEvenDigits to place the largest uncommon digit
+        // in front of the array so that it will skip it next time the for loop with j = i + 1
         temp = *(numberOfUncommonEvenDigits + i);
         *(numberOfUncommonEvenDigits + i) = *(numberOfUncommonEvenDigits + largestUncommonOccurence);
         *(numberOfUncommonEvenDigits + largestUncommonOccurence) = temp;
 
+        //Swap the iPtr element, placing the value with the largest uncommon even digits in front
         temp = *(iPtr + i);
         *(iPtr + i) = *(iPtr + largestUncommonOccurence);
         *(iPtr + largestUncommonOccurence) = temp;
