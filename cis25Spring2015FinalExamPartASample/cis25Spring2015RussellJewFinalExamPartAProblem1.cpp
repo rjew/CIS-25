f27357ab9e35a5cd7c60e165fa817c709c1cce74
Russell Jew <russelljew104@gmail.com>
2015-05-21 03:00:20 -0700

    Program is complete and appears to work.

--- cis25Spring2015FinalExamPartASample/cis25Spring2015RussellJewFinalExamPartAProblem1.cpp
+++ cis25Spring2015FinalExamPartASample/cis25Spring2015RussellJewFinalExamPartAProblem1.cpp
@@ -35,35 +35,34 @@ int main() {
 
     cout << "Ener the size of the array of AnotherBadFraction objects: ";
     cin >> size;
+    cout << endl;
 
     AnotherBadFractionPtr = new AnotherBadFraction[size];
 
     for (i = 0; i < size; i++) {
-        cout << "Enter num: ";
+        cout << "Enter zeroFlag for object #" << i + 1 << ": ";
+        cin >> (AnotherBadFractionPtr + i)->zeroFlag;
+
+        cout << "Enter positiveFlag for object #" << i + 1 << ": ";
+        cin >> (AnotherBadFractionPtr + i)->positiveFlag;
+
+        cout << "Enter num for object #" << i + 1 << ": ";
         cin >> (AnotherBadFractionPtr + i)->num;
 
-        cout << "Enter denom: ";
+        cout << "Enter denom for object #" << i + 1 << ": ";
         cin >> (AnotherBadFractionPtr + i)->denom;
+        cout << endl;
 
         if ((AnotherBadFractionPtr + i)->denom < 0) {
-            (AnotherBadFractionPtr + i)->num = -((AnotherBadFractionPtr + i)->num);
             (AnotherBadFractionPtr + i)->denom = -((AnotherBadFractionPtr + i)->denom);
+            (AnotherBadFractionPtr + i)->num = -((AnotherBadFractionPtr + i)->num);
         }
-
-        (AnotherBadFractionPtr + i)->zeroFlag = ((AnotherBadFractionPtr + i)->denom == 0) ? 1 : 0;
-
-        if ((AnotherBadFractionPtr + i)->num == 0 || (AnotherBadFractionPtr + i)->denom == 0)
-            (AnotherBadFractionPtr + i)->positiveFlag = -1;
-        else if ((AnotherBadFractionPtr + i)->num > 0)
-            (AnotherBadFractionPtr + i)->positiveFlag = 1;
-        else
-            (AnotherBadFractionPtr + i)->positiveFlag = 0;
     }
 
     smallestDigit = analyzeLargestAndSecondLargest(AnotherBadFractionPtr, size);
 
-    cout << "The smallest digit from the largest and second largest AnotherBadFraction objects is "
-        << smallestDigit << "." << endl;
+    cout << "The smallest digit from the largest and second largest \n" 
+        << "AnotherBadFraction objects is " << smallestDigit << "." << endl;
 
     delete[] AnotherBadFractionPtr;
 
@@ -84,15 +83,20 @@ int analyzeLargestAndSecondLargest(AnotherBadFraction* AnotherBadFractionPtr, in
         max = *(AnotherBadFractionPtr + 1);
     }
 
-    for (i = 0; i < size; i++) {
-        if (*(AnotherBadFractionPtr + i) > max) {
-            secondMax = max;
-            max = *(AnotherBadFractionPtr + i);
-        } else {
-            if (*(AnotherBadFractionPtr + i) > secondMax)
-                secondMax = *(AnotherBadFractionPtr + i);
+    for (i = 2; i < size; i++) {
+        if ((AnotherBadFractionPtr + i)->zeroFlag == 0) {
+            if (*(AnotherBadFractionPtr + i) > max) {
+                secondMax = max;
+                max = *(AnotherBadFractionPtr + i);
+            } else {
+                if (*(AnotherBadFractionPtr + i) > secondMax)
+                    secondMax = *(AnotherBadFractionPtr + i);
+            }
         }
     }
+
+    cout << "Max: " << max.num << "/" << max.denom << endl;
+    cout << "Second Max: " << secondMax.num << "/" << secondMax.denom << endl;
 
     extractDigits(secondMax.num, digits);
     extractDigits(secondMax.denom, digits);
@@ -108,9 +112,41 @@ int analyzeLargestAndSecondLargest(AnotherBadFraction* AnotherBadFractionPtr, in
 void extractDigits(int number, int digits[]) {
     int digit;
 
+    number = (number < 0) ? -number : number;
+
     do {
         digit = number % 10;
         digits[digit] = 1;
         number /= 10;
     } while (number != 0);
 }
+
+/* PROGRAM OUTPUT
+Ener the size of the array of AnotherBadFraction objects: 6
+Enter zeroFlag for object #1: 0
+Enter positiveFlag for object #1: 1
+Enter num for object #1: 123
+Enter denom for object #1: 456
+Enter zeroFlag for object #2: 0
+Enter positiveFlag for object #2: 0
+Enter num for object #2: 27
+Enter denom for object #2: -5
+Enter zeroFlag for object #3: 1
+Enter positiveFlag for object #3: -1
+Enter num for object #3: -45
+Enter denom for object #3: 0
+Enter zeroFlag for object #4: 0
+Enter positiveFlag for object #4: 0
+Enter num for object #4: -568
+Enter denom for object #4: 9
+Enter zeroFlag for object #5: 1
+Enter positiveFlag for object #5: -1
+Enter num for object #5: 18
+Enter denom for object #5: 0
+Enter zeroFlag for object #6: 0
+Enter positiveFlag for object #6: -1
+Enter num for object #6: 0
+Enter denom for object #6: 9
+The smallest digit from the largest and second largest AnotherBadFraction object
+s is 0.
+*/
